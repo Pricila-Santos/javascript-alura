17fd41ecd7e4e96ae377ade79af6c84d2f885d98
Pricila santos <pricilaebruno@gmail.com>
2022-09-02 14:53:10 -0300

    Alura Mid

---
+++ main.js
@@ -0,0 +1,5 @@
+function tocaSomPom(){
+    document.querySelector('#som_tecla_pom').play()
+}
+const listaDeTeclas = document.querySelectorAll('.tecla')
+
---
+++ teclas/main.js
@@ -0,0 +1,13 @@
+function tocaSomPom(){
+    document.querySelector('#som_tecla_pom').play()
+}
+const listaDeTeclas =document.querySelectorAll('.tecla')
+let contador = 0
+
+
+while(contador < listaDeTeclas.length) {
+listaDeTeclas[contador].onclick = tocaSomPom
+contador = contador + 1
+console.log (contador)
+
+}
