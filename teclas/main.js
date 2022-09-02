ec6b24116a82e547c4f61187c31b8033f0d5d4dc
Pricila santos <pricilaebruno@gmail.com>
2022-09-02 17:25:51 -0300

    Conclusão Alura Mid

--- teclas/main.js
+++ teclas/main.js
@@ -1,18 +1,37 @@
-function tocaSom(idElementoAudio){
-    document.querySelector(idElementoAudio).play()
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    }
+    else {
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
+
 }
-const listaDeTeclas = document.querySelectorAll('.tecla')
 
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
-for (let contador = 0 ; contador < listaDeTeclas.length;contador ++) {
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
 
-    const tecla = listaDeTeclas[contador]
-    const instrumento = tecla.classList[1]
-    const idAudio = `#som_${instrumento}`
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
 
-    tecla.onclick = function(){ //função anonima
-         tocaSom(idAudio) 
+    tecla.onkeydown = function (evento) {
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
         }
-}
 
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+
+}
