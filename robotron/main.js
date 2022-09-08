ad760f519019c1fa93c9100b6a8ad61dead8a73a
Pricila santos <pricilaebruno@gmail.com>
2022-09-08 11:55:00 -0300

    Exercício

--- robotron/main.js
+++ robotron/main.js
@@ -5,6 +5,6 @@ const braco = document.querySelector('#braco')
 const controle = document.querySelectorAll('.controle-ajuste')
 
 controle.forEach (elemento) => {
-    elemento.addd
+    elemento.add
 }
 
