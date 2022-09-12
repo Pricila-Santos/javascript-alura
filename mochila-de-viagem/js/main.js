7edba4005a927b3212bbf52170653af1f4093e54
Pricila santos <pricilaebruno@gmail.com>
2022-09-12 15:45:06 -0300

    Atualização mochila de viagem

--- mochila-de-viagem/js/main.js
+++ mochila-de-viagem/js/main.js
@@ -1,24 +1,46 @@
  const form = document.getElementById("novoItem")
+ const lista = document.getElementById("lista")
+ const itens = JSON.parse(localStorage.getItem("itens")) || [] // Operador lógico que retorna com dados salvos, ou string vazia, utilizando localStorage.getItem, modificando o valor de `string` com JSON.parse()
+
+ itens.forEach( (elemento) => {// Uso do forEach para que todos os itens já escritos na lista sejam mantidos ao atualizar a página 
+  criaElemento(elemento)
+ });
 
  form.addEventListener("submit",(evento) => {  //qual a operação de um formulario enviado?É o submit(evento)
      evento.preventDefault()  //preventDefault é pra parar o conportamento
      
     
-   criaElemento(evento.target.elements["nome"].value,evento.target.elements["quantidade"].value)
+   const nome = evento.target.elements["nome"]
+   const quantidade = evento.target.elements["quantidade"]
+   
+   const itemAtual ={
+         "nome":nome.value,
+         "quantidade":quantidade.value
+      }
+
+   criaElemento (itemAtual)
+
+   itens.push (itemAtual)
+
+   localStorage.setItem("itens",JSON.stringify(itens))
+
+   nome.value ="" //para deixar o imput limpo sempre que adicionar um item
+   quantidade.value =""
  })
- function criaElemento(nome,quantidade){
- 
+
+ function criaElemento(item){
     const novoItem =document.createElement ('li')
     novoItem.classList.add ("item")  //no meu Html tem uma lista com a classe item
 
     const numeroItem = document.createElement('strong')
-    numeroItem.innerHTML = quantidade
+    numeroItem.innerHTML = item.quantidade
 
     novoItem.appendChild(numeroItem)
-    novoItem.innerHTML += nome
-
-    const lista = document.getElementById("lista")
+    novoItem.innerHTML += item.nome
     lista.appendChild(novoItem)
+    
+    
+    
    
  }
  
